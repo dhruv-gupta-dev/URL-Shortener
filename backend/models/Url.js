47b6c432bbe8a1bbc67a1011ee2a50a73334a8a1
dhruv-gupta-dev <heroicdhruv007@gmail.com>
2026-09-12 09:10:39 +0530

    feat: add URL schema model

---
+++ backend/models/Url.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const urlSchema = new mongoose.Schema({
+    longUrl: {
+        type: String,
+        required: true
+    },
+
+    shortCode: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    clicks: {
+        type: Number,
+        default: 0
+    },
+
+    clickHistory: [
+        {
+            timestamp: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+
+module.exports = mongoose.model('Url',urlSchema);
